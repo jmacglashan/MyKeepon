6024802d0bfa0448ccdd95c667140120a4e4e14f
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-02-07 16:32:38 -0800

    initial commit of a MyKeepon control panel

---
+++ openFrameworks/src/testApp.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxUI.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+		testApp();
+	
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		// main gui
+		ofxUICanvas mainGui;
+		void mainGuiEvent(ofxUIEventArgs &e);
+
+		// panels
+		vector<ofxUICanvas*> thePanels;
+
+		// helpers
+		string fitStringToWidth(const string s, const int w, ofTrueTypeFont ttf);
+};
